f4c6f9d84338c3c5867fe3825690db91c61b8962
Eber <eber4202@hotmail.com>
2021-10-27 01:29:01 -0500

    init project

---
+++ calculator.js
@@ -0,0 +1,17 @@
+'use strict';
+class Calculator {
+   constructor(isFirstNumber = false, number) {
+      this.isFirstNumber = isFirstNumber;
+      this.number = number;
+   }
+
+   toggleNumber() {
+      this.isFirstNumber = !this.isFirstNumber;
+   }
+
+   add() {
+      number 
+   }
+}
+
+export { Calculator };
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import bootstrap from 'bootstrap';
+'use strict';
+import { Calculator } from './calculator.js';
+
+document.addEventListener('DOMContentLoaded', initApp);
+function initApp() {
+   document.querySelectorAll('.btn-primary').forEach(button => button.addEventListener('click', event => alert(event.target.textContent)));
+}
+
