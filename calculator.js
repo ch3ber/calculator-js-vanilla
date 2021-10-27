811f83d47678cc4f77b99acffcb4d86608d42e92
Eber <eber4202@hotmail.com>
2021-10-27 17:04:42 -0500

    finish delete number and add number to display

--- calculator.js
+++ calculator.js
@@ -1,17 +1,59 @@
 'use strict';
-class Calculator {
-   constructor(isFirstNumber = false, number) {
+export class Calculator {
+   constructor(displayId, isFirstNumber, currentSymbol, newSymbol) {
       this.isFirstNumber = isFirstNumber;
-      this.number = number;
+      this.currentSymbol = currentSymbol;
+      this.newSymbol = newSymbol;
+      this.displayId = displayId;
    }
 
-   toggleNumber() {
-      this.isFirstNumber = !this.isFirstNumber;
+   deleteNumber() {
+      const display = document.getElementById(this.displayId);
+      if (display.innerText == 0) return;
+      if (display.innerText != 0) {
+         if (display.innerText.length == 1) {
+            display.innerText = 0;
+         } else {
+            display.innerText = display.innerText.slice(0, -1);
+         }
+      }
+   }
+
+   showResult() {
+      const display = document.getElementById(this.displayId);
+      display.innerText = result;
+   }
+
+   toggleSymbol() {
+      return this.currentSymbol = this.newSymbol;
+   }
+
+   addNumerToDisplay(event) {
+      const display = document.getElementById(this.displayId);
+      (display.innerText == 0)
+         ? display.innerText = event.target.textContent
+         : display.innerText += event.target.textContent;
+   }
+
+}
+
+export class Operations {
+   constructor(firstNumber, secondNumber) {
+      this.firstNumber = firstNumber;
+      this.secondNumber = secondNumber;
    }
 
    add() {
-      number 
+      return this.firstNumber + this.secondNumber;
+   }
+   subtract() {
+      return this.firstNumber - this.secondNumber;
+   }
+   divide() {
+      return this.firstNumber / this.secondNumber;
+   }
+   multiply() {
+      return this.firstNumber * this.secondNumber;
    }
 }
 
-export { Calculator };
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 'use strict';
-import { Calculator } from './calculator.js';
+import { Calculator, Operations } from './calculator.js';
 
-document.addEventListener('DOMContentLoaded', initApp);
-function initApp() {
-   document.querySelectorAll('.btn-primary').forEach(button => button.addEventListener('click', event => alert(event.target.textContent)));
+document.addEventListener('DOMContentLoaded', addEventListeners);
+
+function addEventListeners() {
+
+   // botones numericos
+   document.querySelectorAll('.btn-primary')
+      .forEach(button => button.addEventListener('click', event => calculator.addNumerToDisplay(event) ));
+
+   // botones de operciones
+   document.querySelectorAll('.btn-secondary')
+      .forEach(button => button.addEventListener('click', () => calculator.toggleSymbol() ));
+
+   // boton de resolver
+   document.querySelector('.btn-success')
+      .addEventListener('click', () => calculator.showResult() );
+
+   // boton de borrar un numero
+   document.querySelector('.btn-danger')
+      .addEventListener('click', () => calculator.deleteNumber() );
 }
 
+const calculator = new Calculator('display');
+const operations = new Operations();
